fb4bc61d3ddf466788f5cdbaab3651f923badbac
Manny <manny@His-Mac.local>
2020-02-29 20:19:21 -0500

    FEAT: added mobile detection

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-// IMOPRTS
-import React, { useState, useRef } from "react";
+// IMPORTS
+import React, { useState, useEffect, useRef } from "react";
 import Image from "./img/marvey.online.jpg";
 import Dictionary from "./dictionary.json";
+import MobileDetect from "mobile-detect";
 
 // MAIN COMPONENT
 const App = props => {
+  // States
   const toCopy = useRef(null);
   const [alert, setAlert] = useState(false);
   const [caption, setCaption] = useState("");
   const [converted, setConverted] = useState("");
+  const [userAgent, setUserAgent] = useState(null);
 
+  // Functions
+  /**
+   * Return html text
+   * @param {string} text string to be regex
+   * @returns {object} html copy text
+   */
   const getCaptionShort = text => {
     const re = new RegExp("↵", "g");
     let copyText = text.replace(re, "<br/>");
     return { __html: copyText };
   };
 
-  // const getRegulatText = text => {
-  //   let newText = text;
-  //   for (let i in Dictionary) {
-  //     let re = new RegExp(`${Dictionary[i]["bold-code"]}`, "g");
-  //     newText = newText.replace(re, i);
-  //     re = new RegExp(`${Dictionary[i]["italic-code"]}`, "g");
-  //     newText = newText.replace(re, i);
-  //   }
-  //   return newText;
-  // };
+  /**
+   * Finds regular text version
+   * @param {string} text text that is formatted
+   * @returns {string} text that is unformatted
+   */
+  const getRegulatText = text => {
+    let newText = text;
+    for (let i in Dictionary) {
+      let re = new RegExp(`${Dictionary[i]["bold-code"]}`, "g");
+      newText = newText.replace(re, i);
+      re = new RegExp(`${Dictionary[i]["italic-code"]}`, "g");
+      newText = newText.replace(re, i);
+    }
+    return newText;
+  };
 
+  /**
+   * When form is submitted
+   * @param {Event} event on form submit
+   * @returns {void}
+   */
   const onSubmitForm = event => {
     toCopy.current.select();
     if (toCopy.current.value.length > 0) {
@@ -40,6 +59,11 @@ const App = props => {
     event.preventDefault();
   };
 
+  /**
+   * Function that formats the text for bold or italic
+   * @param {string} value text to be formatted
+   * @returns {void}
+   */
   const formatText = value => {
     let input = window
       .encodeURI(value)
@@ -60,30 +84,55 @@ const App = props => {
     setConverted(input);
   };
 
+  /**
+   * Event handling for textarea onchange
+   * @param {Event} event text area onchange
+   * @returns {void}
+   */
   const onChangeTextarea = event => {
     setCaption(event.target.value);
     formatText(event.target.value);
   };
 
-  //   const formatStyle = style => event => {
-  //     const selection = window.getSelection();
-  //     const start = selection.focusNode.firstElementChild.selectionStart;
-  //     const end = selection.focusNode.firstElementChild.selectionEnd;
-  //     let newCaption = window.decodeURI(
-  //       getRegulatText(window.encodeURI(caption.slice(start, end)))
-  //     );
-  //     for (let i in Dictionary) {
-  //       let re = new RegExp(`${i}`, "g");
-  //       newCaption = newCaption.replace(
-  //         re,
-  //         style === "normal" ? i : Dictionary[i][style]
-  //       );
-  //     }
-  //     newCaption = `${caption.slice(0, start)}${newCaption}${caption.slice(end)}`;
-  //     setCaption(newCaption);
-  //     formatText(newCaption);
-  //     event.preventDefault();
-  //   };
+  /**
+   * Function that formats the style selected
+   * @param {string} style
+   * @returns {void}
+   */
+  const formatStyle = style => event => {
+    const selection = window.getSelection();
+    const start = selection.focusNode.firstElementChild.selectionStart;
+    const end = selection.focusNode.firstElementChild.selectionEnd;
+    let newCaption = window.decodeURI(
+      getRegulatText(window.encodeURI(caption.slice(start, end)))
+    );
+
+    for (let i in Dictionary) {
+      let re = new RegExp(`${i}`, "g");
+      newCaption = newCaption.replace(
+        re,
+        style === "normal" ? i : Dictionary[i][style]
+      );
+    }
+    newCaption = `${caption.slice(0, start)}${newCaption}${caption.slice(end)}`;
+    setCaption(newCaption);
+    formatText(newCaption);
+    event.preventDefault();
+  };
+
+  // Hooks
+  /**
+   * Hook for mobile detection
+   * Remove styling for mobile
+   */
+  useEffect(() => {
+    const md = new MobileDetect(window.navigator.userAgent);
+    if (md.mobile()) {
+      setUserAgent("mobile");
+    } else {
+      setUserAgent("desktop");
+    }
+  }, []);
 
   return (
     <>
@@ -119,33 +168,41 @@ const App = props => {
                 placeholder="Your caption..."
                 value={caption}
               ></textarea>
-              {/* <a
-                id="formatter-normal"
-                href="#normal"
-                onClick={formatStyle("normal")}
-              >
-                n
-              </a>
-              <a id="formatter-bold" href="#bold" onClick={formatStyle("bold")}>
-                b
-              </a>
-              <a
-                id="formatter-bold"
-                href="#italic"
-                onClick={formatStyle("italic")}
-              >
-                i
-              </a>
-              <a id="formatter-emoji" href="#emoji">
-              <span
-                role="img"
-                arial-label="Slightly Smiling Face"
-                aria-labelledby="Slightly Smiling Face"
-              >
-                {" "}
-                ☺
-              </span>
-            </a> */}
+              {userAgent && userAgent === "desktop" && (
+                <>
+                  <a
+                    id="formatter-normal"
+                    href="#normal"
+                    onClick={formatStyle("normal")}
+                  >
+                    n
+                  </a>
+                  <a
+                    id="formatter-bold"
+                    href="#bold"
+                    onClick={formatStyle("bold")}
+                  >
+                    b
+                  </a>
+                  <a
+                    id="formatter-bold"
+                    href="#italic"
+                    onClick={formatStyle("italic")}
+                  >
+                    i
+                  </a>
+                  <a id="formatter-emoji" href="#emoji">
+                    <span
+                      role="img"
+                      arial-label="Slightly Smiling Face"
+                      aria-labelledby="Slightly Smiling Face"
+                    >
+                      {" "}
+                      ☺
+                    </span>
+                  </a>
+                </>
+              )}
             </div>
             <span id="count" className={caption.length > 2200 ? "danger" : ""}>
               {caption.length > 2200 && `(-${caption.length - 2200})`}{" "}
